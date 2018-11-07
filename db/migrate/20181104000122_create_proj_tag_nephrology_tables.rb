978968b99c0b1184a7321629abbad88c447249d5
tibbs001 <sheri.tibbs@duke.edu>
2018-11-06 19:47:57 -0500

    aact-808:  Use a function for creating views. Core AACT needs to drop/recreate views. By providing Core AACT with a function it knows to call to create project views, it doesn’t need to know anything else about Project views - it just knows to call the function create_views

--- db/migrate/20181104000122_create_proj_tag_nephrology_tables.rb
+++ db/migrate/20181104000122_create_proj_tag_nephrology_tables.rb
@@ -14,36 +14,48 @@ class CreateProjTagNephrologyTables < ActiveRecord::Migration[5.2]
       t.string  'mesh_terms'
     end
 
-#    execute <<-SQL
-#      CREATE OR REPLACE VIEW proj_tag_nephrology.tagged_studies AS
-#      select distinct * from studies where nct_id in (
-#        select distinct s.nct_id
-#        from studies s, browse_conditions bc
-#        where s.study_type='Interventional'
-#        and s.study_first_posted_date >= '2007-10-01'
-##        and s.study_first_posted_date < '2018-11-01'
-#        and s.nct_id = bc.nct_id
-#        and bc.downcase_mesh_term IN (select term from proj_tag_nephrology.tagged_terms)
-#
-#        union
-#        select distinct s.nct_id
-#        from studies s, keywords k
-#        where s.study_type='Interventional'
-#        and s.study_first_posted_date >= '2007-10-01'
-#        and s.study_first_posted_date < '2018-11-01'
-#        and s.nct_id = k.nct_id
-#        and k.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)
-#
-#        union
-#        select distinct s.nct_id
-#        from studies s, conditions c
-#        where s.study_type='Interventional'
-#        and s.study_first_posted_date >= '2007-10-01'
-#        and s.study_first_posted_date < '2018-11-01'
-#        and s.nct_id = c.nct_id
-##        and c.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)
-#      );
-#    SQL
+    #  Views that join across schemas can cause problem for pg_restore. It can get hung up on
+    #  dependencies when it's trying to drop/refresh one schema that joins to another
+    #  through a view. To get around this problem, we use functions that create the views.
+    #  When Core AACT restores the Public Database by dumping/restoring the staging database,
+    #  it drops all views before hand. When it's done restoring, it runs a function to recreate
+    #  the view.
+    execute <<-SQL
+        CREATE OR REPLACE FUNCTION proj.create_views() RETURNS void AS
+        $BODY$
+        BEGIN
+          EXECUTE 'CREATE OR REPLACE VIEW proj_tag_nephrology.tagged_studies AS ' ||
+
+            ' select distinct * from ctgov.studies where nct_id in (' ||
+              ' select distinct s.nct_id' ||
+               ' from ctgov.studies s, ctgov.browse_conditions bc ' ||
+               ' where s.study_type=''Interventional''  ' ||
+                 ' and s.study_first_posted_date >= ''2007-10-01'' ' ||
+                 ' and s.study_first_posted_date < ''2018-11-01'' ' ||
+                 ' and s.nct_id = bc.nct_id ' ||
+                 ' and bc.downcase_mesh_term IN (select term from proj_tag_nephrology.tagged_terms) '
+              ' union ' ||
+              '   select distinct s.nct_id ' ||
+              '   from ctgov.studies s, ctgov.keywords k ' ||
+              '   where s.study_type=''Interventional'' ' ||
+              '   and s.study_first_posted_date >= ''2007-10-01'' ' ||
+              '   and s.study_first_posted_date < ''2018-11-01'' ' ||
+              '   and s.nct_id = k.nct_id ' ||
+              '  and k.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)  ' ||
+              ' union  ' ||
+              '   select distinct s.nct_id  ' ||
+              '   from studies s, conditions c  ' ||
+              '   where s.study_type=''Interventional''  ' ||
+              '   and s.study_first_posted_date >= ''2007-10-01''  ' ||
+              '   and s.study_first_posted_date < ''2018-11-01''  ' ||
+              '   and s.nct_id = c.nct_id  ' ||
+              '   and c.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)  ' ||
+              '   ) ';
+
+          RETURN;
+        END;
+        $BODY$ LANGUAGE plpgsql STRICT;
+     SQL
 
   end
 
