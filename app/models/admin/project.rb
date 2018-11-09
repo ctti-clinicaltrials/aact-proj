0b0dd07d592757bf9f478c1726461ce05bf1eea9
tibbs001 <sheri.tibbs@duke.edu>
2018-11-08 23:20:16 -0500

    aact-810:  Add collection of project modules & related convenience methods

--- app/models/admin/project.rb
+++ app/models/admin/project.rb
@@ -3,5 +3,21 @@ module Admin
     has_many :attachments
     has_many :datasets
     has_many :publications
+
+    def self.project_list
+      # A list of all project modules currently in AACT.
+      # Each module (in app/models) encapsulates all info about the project.
+      [ 'TagNephrology', 'Anderson', 'StandardOrgs', 'Tag' ]
+      #[ 'TagNephrology', 'Anderson', 'Tag', 'SummaryTrends', 'Clinwiki', 'Eeg', 'StandardOrgs' ]
+    end
+
+    def self.schema_name_array
+      project_list.map{|p| "Proj#{p}".underscore }
+    end
+
+    def self.schema_name_list
+      schema_name_array.join(', ')
+    end
+
   end
 end
