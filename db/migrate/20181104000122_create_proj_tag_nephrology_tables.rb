67761cf4705a36872b8c075c3cf8da65e338e4b0
tibbs001 <sheri.tibbs@duke.edu>
2018-12-18 14:15:33 -0500

    aact-860:  Comment out code that creates project views.  Will add this in subsequent release.

--- db/migrate/20181104000122_create_proj_tag_nephrology_tables.rb
+++ db/migrate/20181104000122_create_proj_tag_nephrology_tables.rb
@@ -24,52 +24,52 @@ class CreateProjTagNephrologyTables < ActiveRecord::Migration[5.2]
     #  When Core AACT restores the Public Database by dumping/restoring the staging database,
     #  it drops all views before hand. When it's done restoring, it runs a function to recreate
     #  the view.
-    execute <<-SQL
-        CREATE OR REPLACE FUNCTION proj_tag_nephrology.create_views() RETURNS void AS
-        $BODY$
-        BEGIN
-          EXECUTE 'CREATE OR REPLACE VIEW proj_tag_nephrology.tagged_studies AS ' ||
-            ' select distinct s.nct_id, s.start_date, s.start_date_type, s.primary_completion_date, s.primary_completion_date_type, ' ||
-            '   s.acronym, s.brief_title, s.official_title, s.overall_status, s.phase, ' ||
-            '   s.source, s.number_of_arms, s.enrollment, ' ||
-            '   regexp_replace(s.baseline_population, E''[\\n\\r\\u2028]+'', '' '', ''g'' ) as baseline_population,  ' ||
-            '   regexp_replace(s.why_stopped, E''[\\n\\r\\u2028]+'', '' '', ''g'' ) as why_stopped,  ' ||
-            '   regexp_replace(s.limitations_and_caveats, E''[\\n\\r\\u2028]+'', '' '', ''g'' ) as limitations_and_caveats,  ' ||
-            '   cv.number_of_facilities, cv.actual_duration, ' ||
-            '   cv.were_results_reported, cv.has_us_facility, cv.has_single_facility, ' ||
-            '   cv.months_to_report_results , cv.minimum_age_num, cv.minimum_age_unit, ' ||
-            '   cv.maximum_age_num, cv.maximum_age_unit, ' ||
-            '   d.allocation, d.intervention_model, d.primary_purpose, d.time_perspective, d.masking  ' ||
-            '   from ctgov.studies s, ctgov.calculated_values cv, ctgov.designs d ' ||
-            '   where s.nct_id = cv.nct_id ' ||
-            '     and s.nct_id = d.nct_id  ' ||
-            '     and s.nct_id in (        ' ||
-              ' select distinct s.nct_id   ' ||
-               ' from ctgov.studies s, ctgov.browse_conditions bc ' ||
-               ' where s.study_type=''Interventional''  ' ||
-                 ' and s.study_first_posted_date >= ''2007-10-01'' ' ||
-                 ' and s.nct_id = bc.nct_id ' ||
-                 ' and bc.downcase_mesh_term IN (select term from proj_tag_nephrology.tagged_terms) '
-              ' union ' ||
-              '   select distinct s.nct_id ' ||
-              '   from ctgov.studies s, ctgov.keywords k ' ||
-              '   where s.study_type=''Interventional'' ' ||
-              '   and s.study_first_posted_date >= ''2007-10-01'' ' ||
-              '   and s.nct_id = k.nct_id ' ||
-              '  and k.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)  ' ||
-              ' union  ' ||
-              '   select distinct s.nct_id  ' ||
-              '   from studies s, conditions c  ' ||
-              '   where s.study_type=''Interventional''  ' ||
-              '   and s.study_first_posted_date >= ''2007-10-01''  ' ||
-              '   and s.nct_id = c.nct_id  ' ||
-              '   and c.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)  ' ||
-              '   ) ';
-
-          RETURN;
-        END;
-        $BODY$ LANGUAGE plpgsql STRICT;
-     SQL
+    #execute <<-SQL
+    #    CREATE OR REPLACE FUNCTION proj_tag_nephrology.create_views() RETURNS void AS
+    #    $BODY$
+    #    BEGIN
+    #      EXECUTE 'CREATE OR REPLACE VIEW proj_tag_nephrology.tagged_studies AS ' ||
+    #        ' select distinct s.nct_id, s.start_date, s.start_date_type, s.primary_completion_date, s.primary_completion_date_type, ' ||
+    #        '   s.acronym, s.brief_title, s.official_title, s.overall_status, s.phase, ' ||
+    #        '   s.source, s.number_of_arms, s.enrollment, ' ||
+    #        '   regexp_replace(s.baseline_population, E''[\\n\\r\\u2028]+'', '' '', ''g'' ) as baseline_population,  ' ||
+    #        '   regexp_replace(s.why_stopped, E''[\\n\\r\\u2028]+'', '' '', ''g'' ) as why_stopped,  ' ||
+    #        '   regexp_replace(s.limitations_and_caveats, E''[\\n\\r\\u2028]+'', '' '', ''g'' ) as limitations_and_caveats,  ' ||
+    #        '   cv.number_of_facilities, cv.actual_duration, ' ||
+    #        '   cv.were_results_reported, cv.has_us_facility, cv.has_single_facility, ' ||
+    #        '   cv.months_to_report_results , cv.minimum_age_num, cv.minimum_age_unit, ' ||
+    #        '   cv.maximum_age_num, cv.maximum_age_unit, ' ||
+    #        '   d.allocation, d.intervention_model, d.primary_purpose, d.time_perspective, d.masking  ' ||
+    #        '   from ctgov.studies s, ctgov.calculated_values cv, ctgov.designs d ' ||
+    #        '   where s.nct_id = cv.nct_id ' ||
+    #        '     and s.nct_id = d.nct_id  ' ||
+    #        '     and s.nct_id in (        ' ||
+    #          ' select distinct s.nct_id   ' ||
+    #           ' from ctgov.studies s, ctgov.browse_conditions bc ' ||
+    #           ' where s.study_type=''Interventional''  ' ||
+    #             ' and s.study_first_posted_date >= ''2007-10-01'' ' ||
+    #             ' and s.nct_id = bc.nct_id ' ||
+    #             ' and bc.downcase_mesh_term IN (select term from proj_tag_nephrology.tagged_terms) '
+    #          ' union ' ||
+    #          '   select distinct s.nct_id ' ||
+    #          '   from ctgov.studies s, ctgov.keywords k ' ||
+    #          '   where s.study_type=''Interventional'' ' ||
+    #          '   and s.study_first_posted_date >= ''2007-10-01'' ' ||
+    #          '   and s.nct_id = k.nct_id ' ||
+    #          '  and k.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)  ' ||
+    #          ' union  ' ||
+    #          '   select distinct s.nct_id  ' ||
+    #          '   from studies s, conditions c  ' ||
+    #          '   where s.study_type=''Interventional''  ' ||
+    #          '   and s.study_first_posted_date >= ''2007-10-01''  ' ||
+    #          '   and s.nct_id = c.nct_id  ' ||
+    #          '   and c.downcase_name IN (select term from proj_tag_nephrology.tagged_terms)  ' ||
+    #          '   ) ';
+#
+##          RETURN;
+#        END;
+#        $BODY$ LANGUAGE plpgsql STRICT;
+#     SQL
 
   end
 
