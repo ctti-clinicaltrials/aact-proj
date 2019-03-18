5bcc6642a2eccef29c4c8f83e286d3908c5d21de
tibbs001 <sheri.tibbs@duke.edu>
2019-03-18 14:24:30 -0400

    aact-902:  Consistently capitalize clinical specialty names in spreadsheets for both studies and terms.

--- app/models/proj_tag_study_characteristics/oncology_study.rb
+++ app/models/proj_tag_study_characteristics/oncology_study.rb
@@ -9,7 +9,7 @@ module ProjTagStudyCharacteristics
 
     def self.populate_from_file(file_name)
       connection.execute("TRUNCATE TABLE #{table_name};")
-      data = Roo::Spreadsheet.open(file_name).sheet("oncology")
+      data = Roo::Spreadsheet.open(file_name).sheet("Oncology")
       header = data.first.compact.map(&:downcase)
       (2..data.last_row).each  {|i|
         row = Hash[[header, data.row(i)].transpose]
